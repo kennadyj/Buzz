2815cda99ef82dbabf9279133571b027c0744346
kennadyj <kennadyj@users.noreply.github.com>
2017-03-18 23:59:17 -0400

    Add files via upload
    Adding Truck

---
+++ Buzz/Controllers/VehiclesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using static Buzz.Models.Vehicles;
+
+namespace Buzz.Controllers
+{
+    public class VehiclesController : ApiController
+    {
+        Truck[] trucks = new Truck[]
+       {
+
+            new Truck { VIN="ABC12345ZZZ", Color="White", Make="Ford", Model="Explorer" , ModelYear="2017", AllWheelDrive=true},
+            new Truck { VIN="AAAA456JJJJ", Color="Red", Make="Ferrari", Model="F12berlinetta" , ModelYear="2017", AllWheelDrive=false },
+            new Truck { VIN="AXAXAXAX123", Color="Beige", Make="McLaren", Model="570GT" , ModelYear="2017", AllWheelDrive=false }
+       };
+
+        public IEnumerable<Truck> GetAllVehicles()
+        {
+            return trucks;
+        }
+
+        public IHttpActionResult GetVehicle(string id)
+        {
+            var product = trucks.FirstOrDefault((p) => p.VIN == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ Buzz/Models/Vehicles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Buzz.Models
+{
+    public class Vehicles
+    {
+        public class Truck
+        {
+            public string VIN { get; set; }
+            public string Model { get; set; }
+            public string Make { get; set; }
+            public string ModelYear { get; set; }
+            public string Color { get; set; }
+            public bool AllWheelDrive { get; set; }
+        }
+    }
+}
